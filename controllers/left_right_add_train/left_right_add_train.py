c4e881d4397db28369e45beac4d309c9a89fa70a
Muros-lab <muros4073@gmail.com>
2023-07-06 00:56:14 +0900

    [Update] reward detail focusing action

--- controllers/left_right_add_train/left_right_add_train.py
+++ controllers/left_right_add_train/left_right_add_train.py
@@ -1,6 +1,6 @@
 STATE_SIZE = 24
 MAX_SPEED = 0.3725
-MAX_EPISODE = 100
+MAX_EPISODE = 120
 MAX_FRAME = 3
 MAX_LENGHT = 0.9
 MIN_DISTANCE = 0.35
@@ -11,7 +11,7 @@ REPLAY_CYCLE = 2000
 TARGET_NETWORK_CYCLE = 5
 GOAL_X = 0
 GOAL_Y = 0
-MODIFY_NUM = 18
+MODIFY_NUM = 19
 
 import os
 import math
@@ -123,29 +123,29 @@ def Action(action):
         right_motor.setVelocity(MAX_SPEED)
     elif action == 1:
         left_motor.setVelocity(MAX_SPEED)
-        right_motor.setVelocity(0)
+        right_motor.setVelocity(-MAX_SPEED)
     elif action == 2:
-        left_motor.setVelocity(0)
+        left_motor.setVelocity(-MAX_SPEED)
         right_motor.setVelocity(MAX_SPEED)
 
 # 2-4. Reward structure
 def Reward(state,next_state):
-    total = 0                                               
+    total = 0
     for i in range(MAX_FRAME):                                 
         if next_state[i * input] < ARRIVE_STANDARD:
-            total += 50
-        if state[i * input + 2] < 0.8 and state[i * input + 3] < 0.8 and state[i * input + 4] < 0.8 and state[i * input + 5] < 0.8 and state[i * input + 6] < 0.8 and state[i * input + 7] < 0.8:
-            for qq in range(50):
-                if next_state[i * input] + 0.00071 - qq * 0.000005 < state[i * input]:
-                    total += 0.5
-            for qq in range(50):
-                if state[i * input] + 0.00071 - qq * 0.000005 < next_state[i * input]:
-                    total -= 0.1
-        if state[i * input + 2] < 0.8 and state[i * input + 3] < 0.8 and state[i * input + 4] < 0.8 and state[i * input + 5] < 0.8 and state[i * input + 6] < 0.8 and state[i * input + 7] < 0.8:
-             if abs(next_state[i * input + 1]) < abs(state[i * input + 1]) and action != 0:
-                 total += 0.1
-        if abs(next_state[i * input + 1]) > 160:
-            total -= 0.5
+            total += 10
+        if state[i * input + 1] < 0 and action == 2:
+            total += 0.01
+        if state[i * input + 1] < 0 and action == 1:
+            total -= 0.01
+        if state[i * input + 1] > 0 and action == 1:
+            total += 0.01
+        if state[i * input + 1] > 0 and action == 2:
+            total -= 0.01
+        if state[i * input] > next_state[i * input] and action == 0:
+            total += 0.01
+        if state[i * input] < next_state[i * input] and action == 0:
+            total -= 0.01
     return total
     
 # 2.5. Done check

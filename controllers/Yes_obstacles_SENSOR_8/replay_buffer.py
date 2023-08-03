9761083e8ba36da294c44e5b794f0359c5ffc0d3
wntdevelop <chjm0756@tukorea.ac.kr>
2023-08-03 12:02:34 +0900

    pipe world model 7 good

--- controllers/Yes_obstacles_SENSOR_8/replay_buffer.py
+++ controllers/Yes_obstacles_SENSOR_8/replay_buffer.py
@@ -1,6 +1,6 @@
 REPALY_MEMORY = 1000000
 MIN_BATCH_SIZE = 1024
-CL_NAME = "CL_YES_world_easy_action 5_6"
+CL_NAME = "CL_YES_world_easy_action 5_7"
 CL_LOAD_NAME = "CL_No_world_action 5"
 
 import random

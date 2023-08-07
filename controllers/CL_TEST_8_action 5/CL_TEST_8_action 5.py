ec2983bbeed6d6f55701cc9a1ab917e4ce962fbc
wntdevelop <chjm0756@tukorea.ac.kr>
2023-08-07 17:17:50 +0900

    model test -> gap11, deadlock 50% success 50% -> CL로 개선해야함

--- controllers/CL_TEST_8_action 5/CL_TEST_8_action 5.py
+++ controllers/CL_TEST_8_action 5/CL_TEST_8_action 5.py
@@ -1,5 +1,5 @@
 CL_MODEL = "Curriculum Yes ob pipe_section 0_2"
-CL_KIND = f"{CL_MODEL}_test_pipe_section_13(gap)"
+CL_KIND = f"{CL_MODEL}_test_pipe_section_11(gap)"
 TEST_COUNT = 10
 INPUT_SENSOR = 8
 INPUT_SIZE = 10
